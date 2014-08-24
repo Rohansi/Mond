6a63e390cc153d2722ac31803b55bfce299569c2
Rohan Singh <rohan-singh@hotmail.com>
2014-08-23 20:14:55 -0400

    Allow creation of lambda expressions without fun keyword

--- Mond/Compiler/Parselets/GroupParselet.cs
+++ Mond/Compiler/Parselets/GroupParselet.cs
@@ -1,4 +1,6 @@
-﻿using Mond.Compiler.Expressions;
+﻿using System.Collections.Generic;
+using Mond.Compiler.Expressions;
+using Mond.Compiler.Expressions.Statements;
 
 namespace Mond.Compiler.Parselets
 {
@@ -6,9 +8,54 @@ namespace Mond.Compiler.Parselets
     {
         public Expression Parse(Parser parser, Token token)
         {
+            if ((parser.Match(TokenType.RightParen) && parser.Match(TokenType.Pointy, 1)) ||                                            // () ->
+                (parser.Match(TokenType.Identifier) && parser.Match(TokenType.RightParen, 1) && parser.Match(TokenType.Pointy, 2)) ||   // (ident) ->
+                (parser.Match(TokenType.Identifier) && parser.Match(TokenType.Comma, 1)) ||                                             // (ident,
+                 parser.Match(TokenType.Ellipsis))                                                                                      // (...
+            {
+                return ParseLambdaExpression(parser, token);
+            }
+
             var expression = parser.ParseExpession();
             parser.Take(TokenType.RightParen);
             return expression;
+        }
+
+        private static Expression ParseLambdaExpression(Parser parser, Token token)
+        {
+            var arguments = new List<string>();
+            string otherArgs = null;
+
+            // parse argument list
+            if (!parser.Match(TokenType.RightParen))
+            {
+                while (true)
+                {
+                    if (parser.MatchAndTake(TokenType.Ellipsis))
+                    {
+                        otherArgs = parser.Take(TokenType.Identifier).Contents;
+                        break;
+                    }
+
+                    var identifier = parser.Take(TokenType.Identifier);
+                    arguments.Add(identifier.Contents);
+
+                    if (parser.Match(TokenType.RightParen))
+                        break;
+
+                    parser.Take(TokenType.Comma);
+                }
+            }
+
+            parser.Take(TokenType.RightParen);
+            parser.Take(TokenType.Pointy);
+
+            var body = new BlockExpression(new List<Expression>
+            {
+                new ReturnExpression(token, parser.ParseExpession())
+            });
+
+            return new FunctionExpression(token, null, arguments, otherArgs, body);
         }
     }
 }
