725128b96a3e48cf964d2a1028d46c73723cb37a
Tony Montana <js.pwns@gmail.com>
2014-10-28 18:08:10 -0400

    Some new methods for MondProgram.
    * Enable MondProgram to read source files directly via the FromFile static method.
    * Enable compiled programs to be saved to a binary file via the Dump instance method.
    * Enable pre-compiled programs to be loaded from binary files via the Load static method.
    * Add internal properties to get the Functions and Lines from a DebugInfo instance to allow them to be written to the file.

--- Mond/MondProgram.cs
+++ Mond/MondProgram.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Text;
 using Mond.Compiler;
 using Mond.Compiler.Expressions;
 using Mond.VirtualMachine;
@@ -9,6 +11,9 @@ namespace Mond
 {
     public sealed class MondProgram
     {
+        private const uint MAGIC_ID = 0xFA57C0DE;
+        private const byte FORMAT_VERSION = 1;
+
         internal readonly byte[] Bytecode;
         internal readonly List<MondValue> Numbers;
         internal readonly List<MondValue> Strings;
@@ -20,6 +25,153 @@ namespace Mond
             Numbers = numbers.Select(n => new MondValue(n)).ToList();
             Strings = strings.Select(s => new MondValue(s)).ToList();
             DebugInfo = debugInfo;
+        }
+
+        /// <summary>
+        /// Writes the compiled Mond program to the specified file.
+        /// </summary>
+        /// <param name="path">File to write the bytecode to.</param>
+        public void Dump(string path)
+        {
+            using (var fs = File.Open(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                Dump(fs);
+            }
+        }
+
+        /// <summary>
+        /// Writes the compiled Mond program to the specified stream.
+        /// </summary>
+        /// <param name="output">Stream to write to.</param>
+        public void Dump(Stream output)
+        {
+            using (var writer = new BinaryWriter(output, Encoding.UTF8, true))
+            {
+                writer.Write(MAGIC_ID);
+                writer.Write(FORMAT_VERSION);
+                writer.Write(DebugInfo != null);
+                writer.Write(Strings.Count);
+
+                foreach (var str in Strings)
+                    writer.Write(str.ToString());
+
+                writer.Write(Numbers.Count);
+
+                foreach (var num in Numbers)
+                    writer.Write((double)num);
+
+                writer.Write(Bytecode.Length);
+                writer.Write(Bytecode, 0, Bytecode.Length);
+
+                if (DebugInfo != null)
+                {
+                    writer.Write(DebugInfo.Functions.Count);
+
+                    foreach (var function in DebugInfo.Functions)
+                    {
+                        writer.Write(function.Address);
+                        writer.Write(function.Name);
+                    }
+
+                    writer.Write(DebugInfo.Lines.Count);
+
+                    foreach (var line in DebugInfo.Lines)
+                    {
+                        writer.Write(line.Address);
+                        writer.Write(line.FileName);
+                        writer.Write(line.LineNumber);
+                    }
+                }
+
+                writer.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Load a Mond source code from a file and return the compiled program.
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
+        /// <param name="options">Compiler options.</param>
+        public static MondProgram FromFile(string path, MondCompilerOptions options = null)
+        {
+            var source = File.ReadAllText(path, Encoding.UTF8);
+            var name = Path.GetFileName(path);
+            return Compile(source, name, options);
+        }
+
+        /// <summary>
+        /// Loads Mond bytecode from the specified file.
+        /// </summary>
+        /// <param name="path">The file to load.</param>
+        public static MondProgram Load(string path)
+        {
+            using (var fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return Load(fs);
+            }
+        }
+
+        /// <summary>
+        /// Loads Mond bytecode from the specified stream.
+        /// </summary>
+        /// <param name="stream">Stream to read from.</param>
+        public static MondProgram Load(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
+            {
+                if (reader.ReadUInt32() != MAGIC_ID)
+                    throw new NotSupportedException("Input is not valid.");
+
+                byte version;
+                if ((version = reader.ReadByte()) != FORMAT_VERSION)
+                    throw new NotSupportedException(string.Format("Wrong version. Expected {0:X2}, got {1:X2}.", FORMAT_VERSION, version));
+
+                var hasDebugInfo = reader.ReadBoolean();
+
+                var stringCount = reader.ReadInt32();
+                var strings = new List<string>();
+
+                for (var i = 0; i < stringCount; ++i)
+                    strings.Add(reader.ReadString());
+
+                var numberCount = reader.ReadInt32();
+                var numbers = new List<double>();
+
+                for (var i = 0; i < numberCount; ++i)
+                    numbers.Add(reader.ReadDouble());
+
+                var bytecodeLength = reader.ReadInt32();
+                var bytecode = reader.ReadBytes(bytecodeLength);
+
+                var debugInfo = (DebugInfo)null;
+                if (hasDebugInfo)
+                {
+                    var functionCount = reader.ReadInt32();
+                    var functions = new List<DebugInfo.Function>();
+
+                    for (var i = 0; i < functionCount; ++i)
+                    {
+                        var address = reader.ReadInt32();
+                        var name = reader.ReadInt32();
+                        var function = new DebugInfo.Function(address, name);
+                        functions.Add(function);
+                    }
+
+                    var lineCount = reader.ReadInt32();
+                    var lines = new List<DebugInfo.Line>();
+
+                    for (var i = 0; i < lineCount; ++i)
+                    {
+                        var address = reader.ReadInt32();
+                        var fileName = reader.ReadInt32();
+                        var lineNumber = reader.ReadInt32();
+                        var line = new DebugInfo.Line(address, fileName, lineNumber);
+                        lines.Add(line);
+                    }
+                }
+
+                return new MondProgram(bytecode, numbers, strings, debugInfo);
+            }
         }
 
         /// <summary>
