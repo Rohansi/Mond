69ba269f8d9a0ed537353caaf446a7096e88d398
Rohan Singh <rohan-singh@hotmail.com>
2015-05-25 11:42:53 -0400

    Fix nuspec version

--- Mond.RemoteDebugger/Properties/AssemblyInfo.cs
+++ Mond.RemoteDebugger/Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.0")]
 [assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-pre")]
