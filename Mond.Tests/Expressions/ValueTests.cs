aa03924688ddabcc55f1d3f16fb9c7c5decf0216
Rohan Singh <rohan-singh@hotmail.com>
2017-06-15 20:57:00 -0400

    Start breaking up large test cases

--- Mond.Tests/Expressions/ValueTests.cs
+++ Mond.Tests/Expressions/ValueTests.cs
@@ -8,36 +8,22 @@ namespace Mond.Tests.Expressions
         private MondValue _result;
 
         [Test]
-        public void NumberParse()
-        {
-            Assert.True(Script.Run("return 100;") == 100, "simple");
-
-            Assert.True(Script.Run("return 1_000;") == 1000, "simple with separator");
-
-            Assert.True(Script.Run("return 100.35;") == 100.35, "decimal");
-
-            Assert.True(Script.Run("return -100;") == -100, "sign");
-
-            Assert.True(Script.Run("return 10e4;") == 10e4, "exponent");
-
-            Assert.True(Script.Run("return 10e40;") == 10e40, "big exponent");
-
-            Assert.True(Script.Run("return 10e+4;") == 10e+4, "exponent sign +");
-
-            Assert.True(Script.Run("return 10e-4;") == 10e-4, "exponent sign -");
-
-            Assert.True(Script.Run("return 0x1234ABCD;") == 0x1234ABCD, "hex number");
-
-            Assert.True(Script.Run("return 0x1234_ABCD;") == 0x1234ABCD, "hex number with separator");
-
-            Assert.True(Script.Run("return 0b00010010001101001010101111001101;") == 0x1234ABCD, "binary number");
-
-            Assert.True(Script.Run("return 0b00010010_00110100_10101011_11001101;") == 0x1234ABCD, "binary number with separator");
-
-            Assert.True(Script.Run("return 0xDEADBEEF;") == unchecked((int)0xDEADBEEF), "negative hex number");
-
-            Assert.True(Script.Run("return 0b11011110101011011011111011101111;") == unchecked((int)0xDEADBEEF), "negative binary number");
-        }
+        [TestCase("100", 100)]
+        [TestCase("1_000", 1000)]
+        [TestCase("100.35", 100.35)]
+        [TestCase("-100", -100)]
+        [TestCase("10e4", 10e4)]
+        [TestCase("10e40", 10e40)]
+        [TestCase("10e+4", 10e+4)]
+        [TestCase("10e-4", 10e-4)]
+        [TestCase("0x1234ABCD", 0x1234ABCD)]
+        [TestCase("0x1234_ABCD", 0x1234ABCD)]
+        [TestCase("0b00010010001101001010101111001101", 0x1234ABCD)]
+        [TestCase("0b00010010_00110100_10101011_11001101", 0x1234ABCD)]
+        [TestCase("0xDEADBEEF", unchecked((int)0xDEADBEEF))]
+        [TestCase("0b11011110101011011011111011101111", unchecked((int)0xDEADBEEF))]
+        public void NumberParse(string expression, double expected) =>
+            Assert.AreEqual((MondValue)expected, Script.Run($"return {expression};"));
 
         [Test]
         public void Add()
@@ -47,20 +33,43 @@ namespace Mond.Tests.Expressions
                 return a + b;
             ");
 
-            Assert.True(_result == 110);
+            Assert.AreEqual((MondValue)110, _result);
+        }
 
+        [Test]
+        public void AddImplicitToStringRight()
+        {
             _result = Script.Run(@"
                 var a = 'test', b = 10;
                 return a + b;
             ");
 
-            Assert.True(_result == "test10");
+            Assert.AreEqual((MondValue)"test10", _result);
+        }
 
+        [Test]
+        public void AddImplicitToStringLeft()
+        {
+            _result = Script.Run(@"
+                var a = 10, b = 'test';
+                return a + b;
+            ");
+
+            Assert.AreEqual((MondValue)"10test", _result);
+        }
+
+        [Test]
+        public void AddInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = null, b = 10;
                 return a + b;
             "));
+        }
 
+        [Test]
+        public void AddAssignmentNotStorable()
+        {
             Assert.Throws<MondCompilerException>(() => Script.Run(@"
                 return 1 += 2;
             "));
@@ -74,8 +83,12 @@ namespace Mond.Tests.Expressions
                 return a - b;
             ");
 
-            Assert.True(_result == 90);
+            Assert.AreEqual((MondValue)90, _result);
+        }
 
+        [Test]
+        public void SubtractInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 10;
                 return a - b;
@@ -90,8 +103,12 @@ namespace Mond.Tests.Expressions
                 return a * b;
             ");
 
-            Assert.True(_result == 1000);
+            Assert.AreEqual((MondValue)1000, _result);
+        }
 
+        [Test]
+        public void MultiplyInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 10;
                 return a * b;
@@ -106,8 +123,12 @@ namespace Mond.Tests.Expressions
                 return a / b;
             ");
 
-            Assert.True(_result == 10);
+            Assert.AreEqual((MondValue)10, _result);
+        }
 
+        [Test]
+        public void DivideInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 10;
                 return a / b;
@@ -122,8 +143,12 @@ namespace Mond.Tests.Expressions
                 return a % b;
             ");
 
-            Assert.True(_result == 1);
+            Assert.AreEqual((MondValue)1, _result);
+        }
 
+        [Test]
+        public void ModuloInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 10;
                 return a % b;
@@ -138,8 +163,12 @@ namespace Mond.Tests.Expressions
                 return a ** b;
             ");
 
-            Assert.True(_result == 256);
+            Assert.AreEqual((MondValue)256, _result);
+        }
 
+        [Test]
+        public void ExponentInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 10;
                 return a ** b;
@@ -154,8 +183,12 @@ namespace Mond.Tests.Expressions
                 return a << b;
             ");
 
-            Assert.True(_result == 32);
+            Assert.AreEqual((MondValue)32, _result);
+        }
 
+        [Test]
+        public void LeftShiftInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 4;
                 return a << b;
@@ -170,8 +203,12 @@ namespace Mond.Tests.Expressions
                 return a >> b;
             ");
 
-            Assert.True(_result == 16);
+            Assert.AreEqual((MondValue)16, _result);
+        }
 
+        [Test]
+        public void RightShiftInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 2;
                 return a << b;
@@ -186,8 +223,12 @@ namespace Mond.Tests.Expressions
                 return a | b;
             ");
 
-            Assert.True(_result == 6);
+            Assert.AreEqual((MondValue)6, _result);
+        }
 
+        [Test]
+        public void BitOrInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 4;
                 return a | b;
@@ -202,8 +243,12 @@ namespace Mond.Tests.Expressions
                 return a & b;
             ");
 
-            Assert.True(_result == 4);
+            Assert.AreEqual((MondValue)4, _result);
+        }
 
+        [Test]
+        public void BitAndInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 4;
                 return a & b;
@@ -218,8 +263,12 @@ namespace Mond.Tests.Expressions
                 return a ^ b;
             ");
 
-            Assert.True(_result == 2);
+            Assert.AreEqual((MondValue)2, _result);
+        }
 
+        [Test]
+        public void BitXorInvalidType()
+        {
             Assert.Throws<MondRuntimeException>(() => Script.Run(@"
                 var a = 'test', b = 4;
                 return a ^ b;
@@ -227,52 +276,124 @@ namespace Mond.Tests.Expressions
         }
 
         [Test]
-        public void Increment()
+        public void IncrementSuffix()
+        {
+            _result = Script.Run(@"
+                var a = 0;
+                a++;
+                return a;
+            ");
+
+            Assert.AreEqual((MondValue)1, _result);
+        }
+
+        [Test]
+        public void IncrementSuffixResult()
         {
             _result = Script.Run(@"
                 var a = 0;
                 return a++;
             ");
 
-            Assert.True(_result == 0);
+            Assert.AreEqual((MondValue)0, _result);
+        }
 
+        [Test]
+        public void IncrementSuffixNotStorable()
+        {
             Assert.Throws<MondCompilerException>(() => Script.Run(@"
                 return 1++;
             "));
+        }
 
+        [Test]
+        public void IncrementPrefix()
+        {
+            _result = Script.Run(@"
+                var a = 0;
+                ++a;
+                return a;
+            ");
+
+            Assert.AreEqual((MondValue)1, _result);
+        }
+
+        [Test]
+        public void IncrementPrefixResult()
+        {
             _result = Script.Run(@"
                 var a = 0;
                 return ++a;
             ");
 
-            Assert.True(_result == 1);
+            Assert.AreEqual((MondValue)1, _result);
+        }
 
+        [Test]
+        public void IncrementPrefixNotStorable()
+        {
             Assert.Throws<MondCompilerException>(() => Script.Run(@"
                 return ++1;
             "));
         }
 
         [Test]
-        public void Decrement()
+        public void DecrementSuffix()
+        {
+            _result = Script.Run(@"
+                var a = 0;
+                a--;
+                return a;
+            ");
+
+            Assert.AreEqual((MondValue)(-1), _result);
+        }
+
+        [Test]
+        public void DecrementSuffixResult()
         {
             _result = Script.Run(@"
                 var a = 0;
                 return a--;
             ");
 
-            Assert.True(_result == 0);
+            Assert.AreEqual((MondValue)0, _result);
+        }
 
+        [Test]
+        public void DecrementSuffixNotStorable()
+        {
             Assert.Throws<MondCompilerException>(() => Script.Run(@"
                 return 1--;
             "));
+        }
 
+        [Test]
+        public void DecrementPrefix()
+        {
+            _result = Script.Run(@"
+                var a = 0;
+                --a;
+                return a;
+            ");
+
+            Assert.AreEqual((MondValue)(-1), _result);
+        }
+
+        [Test]
+        public void DecrementPrefixResult()
+        {
             _result = Script.Run(@"
                 var a = 0;
                 return --a;
             ");
 
-            Assert.True(_result == -1);
+            Assert.AreEqual((MondValue)(-1), _result);
+        }
 
+        [Test]
+        public void DecrementPrefixNotStorable()
+        {
             Assert.Throws<MondCompilerException>(() => Script.Run(@"
                 return --1;
             "));
@@ -281,19 +402,19 @@ namespace Mond.Tests.Expressions
         [Test]
         public void Constants()
         {
-            Assert.True(Script.Run("return null;") == MondValue.Null);
+            Assert.AreEqual(MondValue.Null, Script.Run("return null;"));
 
-            Assert.True(Script.Run("return undefined;") == MondValue.Undefined);
+            Assert.AreEqual(MondValue.Undefined, Script.Run("return undefined;"));
+
+            Assert.AreEqual(MondValue.True, Script.Run("return true;"));
+
+            Assert.AreEqual(MondValue.False, Script.Run("return false;"));
 
             Assert.True(double.IsNaN(Script.Run("return NaN;")));
 
             Assert.True(double.IsInfinity(Script.Run("return Infinity;")));
 
             Assert.True(double.IsNegativeInfinity(Script.Run("return -Infinity;")));
-
-            Assert.True(Script.Run("return true;"));
-
-            Assert.False(Script.Run("return false;"));
         }
 
         [Test]
@@ -304,7 +425,7 @@ namespace Mond.Tests.Expressions
                 return -a;
             ");
 
-            Assert.True(_result == -100);
+            Assert.AreEqual((MondValue)(-100), _result);
         }
 
         [Test]
@@ -315,7 +436,7 @@ namespace Mond.Tests.Expressions
                 return ~a;
             ");
 
-            Assert.True(_result == -101);
+            Assert.AreEqual((MondValue)(-101), _result);
         }
     }
 }
