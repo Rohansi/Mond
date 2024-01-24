9baeb0e9e2a84899da0bd74871da4364a9b67f81
Rohan Singh <rohan-singh@hotmail.com>
2024-01-24 10:44:50 -0500

    Add some code from a while ago

---
+++ Mond.SourceGenerator/MondSourceGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace Mond.SourceGenerator
+{
+    [Generator]
+    public class MondSourceGenerator : ISourceGenerator
+    {
+        private const string Category = "Mond";
+
+        private static readonly DiagnosticDescriptor MissingSyntaxReceiver = new DiagnosticDescriptor(
+            "MOND001",
+            "Internal error - syntax receiver is null",
+            "The syntax receiver was not set or is an unexpected type - cannot generate Mond bindings.", Category,
+            DiagnosticSeverity.Error, true);
+
+        private static readonly DiagnosticDescriptor CannotBindGeneric = new DiagnosticDescriptor(
+            "MOND002",
+            "Cannot generate Mond bindings for generic types",
+            "Open generic types cannot be bound to Mond. Either remove the generic parameters from this or bind closed types which derive from this type instead.", Category,
+            DiagnosticSeverity.Error, true);
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!(context.SyntaxContextReceiver is SyntaxReceiver syntaxReceiver))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(MissingSyntaxReceiver, Location.None));
+                return;
+            }
+
+            // https://stackoverflow.com/questions/64623689/get-all-types-from-compilation-using-roslyn
+            context.Compilation.GlobalNamespace.GetMembers();
+
+            foreach (var module in syntaxReceiver.Modules)
+            {
+                if (module.Arity != 0)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(CannotBindGeneric, module.Locations.First()));
+                    continue;
+                }
+            }
+
+            foreach (var klass in syntaxReceiver.Classes)
+            {
+                if (klass.Arity != 0)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(CannotBindGeneric, klass.Locations.First()));
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Mond.SourceGenerator/SyntaxReceiver.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Mond.SourceGenerator
+{
+    internal class SyntaxReceiver : ISyntaxContextReceiver
+    {
+        public List<INamedTypeSymbol> Modules { get; } = new List<INamedTypeSymbol>();
+        public List<INamedTypeSymbol> Classes { get; } = new List<INamedTypeSymbol>();
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax classDecl)
+            {
+                var symbol = context.SemanticModel.GetDeclaredSymbol(classDecl);
+                if (!(symbol is INamedTypeSymbol classSymbol))
+                {
+                    // todo: can we log somewhere?
+                    return;
+                }
+
+                var attributes = classSymbol.GetAttributes();
+                var isModule = HasAttribute(attributes, "MondModuleAttribute");
+                var isClass = HasAttribute(attributes, "MondClassAttribute");
+                
+                if (isModule)
+                {
+                    Modules.Add(classSymbol);
+                }
+                else if (isClass)
+                {
+                    Classes.Add(classSymbol);
+                }
+            }
+        }
+
+        private static bool HasAttribute(ImmutableArray<AttributeData> attributes, string name)
+        {
+            return attributes.Any(a =>
+                a.AttributeClass != null &&
+                a.AttributeClass.ContainingNamespace?.Name == "Mond.Binding" &&
+                a.AttributeClass.Name == name);
+        }
+    }
+}
